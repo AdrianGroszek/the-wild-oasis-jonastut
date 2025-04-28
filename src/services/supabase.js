00cf510cf5e3e428fef1d59b19bd3e5e62d0a5f3
Adrian <adrian.grochowski17@gmail.com>
2025-04-28 09:32:11 +0200

    Added backend (supabase)

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://tfqnszhpdkrkoycflcpd.supabase.co';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmcW5zemhwZGtya295Y2ZsY3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NzUzMzIsImV4cCI6MjA2MTE1MTMzMn0.DWAyMtq9XWMpH9HzzcPGbd-Yvy4sZPznEtQ3adJzqiA';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
