7435a0ab4ac0912b8eb17f521beb51e81cc5a80a
Adrian <adrian.grochowski17@gmail.com>
2025-05-19 17:25:17 +0200

    Added vercel file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://tfqnszhpdkrkoycflcpd.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmcW5zemhwZGtya295Y2ZsY3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NzUzMzIsImV4cCI6MjA2MTE1MTMzMn0.DWAyMtq9XWMpH9HzzcPGbd-Yvy4sZPznEtQ3adJzqiA';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
